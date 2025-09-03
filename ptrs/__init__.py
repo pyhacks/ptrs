ed43b8f3eee33efb5b1fee27d9c2ac3b33f7a07f
pyhacks <enginyildirim111@gmail.com>
2025-09-03 23:31:59 +0300

    Add files via upload

---
+++ ptrs/__init__.py
@@ -0,0 +1,5 @@
+from .ptrs import pointer_to_object, pointer_to_address, \
+                  dereference, address_of, \
+                  mem_read, mem_write, \
+                  mem_restore, mem_restore_last
+                  
